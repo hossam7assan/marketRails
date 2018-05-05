e8e5df300471c250fd191ce58cca740ee8cf44c2
hossam7assan <hos.hassan.999@gmail.com>
2018-05-05 15:26:46 +0200

    add validation method on product model

--- market/app/models/product.rb
+++ market/app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ApplicationRecord
     belongs_to :category
+
+    validate :price_limits
+
+    private
+
+    def price_limits
+        if price < 0
+            errors.add(:price, "price cannot be 0 or less")
+        end
+      end
 end
